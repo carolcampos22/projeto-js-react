ec300230c9242856508a1361ccf2e3d513447e92
carolcampos22 <mendes.carolina2209@gmail.com>
2023-07-20 22:03:31 -0300

    correção de bugs

--- src/Components/Footer/footerStyled.js
+++ src/Components/Footer/footerStyled.js
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.div`
-   
-    color: black;
     display: flex;
+    justify-content: space-around;
+    color: black;
     justify-content: center;
     padding: 1vh;
+    background-color: lightblue;
+    div{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 5px;
+        gap: 5vw;
+    }
    
 `
 
